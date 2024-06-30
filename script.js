bcebddad9e5bc6a7b188fb5ad1927153bee986c4
Malibu Studios <147752397+Olguixx@users.noreply.github.com>
2024-06-30 18:35:06 +0200

    Cambio definitivo 1
    and i opp

--- script.js
+++ script.js
@@ -1,46 +1,53 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Mostrar la pestaña de "Campaña" por defecto al cargar la página
     document.getElementById("tab1").classList.add("active");
 });
 
 function openTab(event, tabId) {
-    // Ocultar todos los contenidos de las pestañas
     var tabContents = document.getElementsByClassName("tab-content");
     for (var i = 0; i < tabContents.length; i++) {
         tabContents[i].classList.remove("active");
     }
     
-    // Mostrar el contenido de la pestaña seleccionada
     var selectedTab = document.getElementById(tabId);
     selectedTab.classList.add("active");
 }
 
 window.addEventListener('scroll', function() {
     var leftColumn = document.getElementById('leftColumn');
-    var scrollPosition = window.scrollY;
+    var triggerElement = document.getElementById('centerColumn');
+    var triggerPosition = triggerElement.getBoundingClientRect().top;
 
-    // Ajustar la posición de la columna izquierda
-    if (scrollPosition >= 1195) { // 50px es la distancia desde la parte superior
+    if (triggerPosition <= 0) { 
         leftColumn.style.position = 'fixed';
         leftColumn.style.top = '0';
     } else {
         leftColumn.style.position = 'absolute';
-        leftColumn.style.top = '0'; // 50px es la distancia desde la parte superior
+        leftColumn.style.top = '0';
     }
 });
 
 window.addEventListener('scroll', function() {
     var leftColumn = document.getElementById('leftColumn2');
-    var scrollPosition = window.scrollY;
+    var triggerElement = document.getElementById('centerColumn2');
+    var triggerPosition = triggerElement.getBoundingClientRect().top;
 
-    // Ajustar la posición de la columna izquierda
-    if (scrollPosition >= 900) { // 50px es la distancia desde la parte superior
+    if (triggerPosition <= 0) { 
         leftColumn.style.position = 'fixed';
         leftColumn.style.top = '0';
     } else {
         leftColumn.style.position = 'absolute';
-        leftColumn.style.top = '0'; // 50px es la distancia desde la parte superior
+        leftColumn.style.top = '0';
     }
 });
 
+window.addEventListener('scroll', function(){
+    var barritas = document.getElementById('barritas');
+    var scrollPosition = window.scrollY;
+    var hvideo = document.getElementById('video').style.height;
 
+    if (scrollPosition >= 650 + hvideo){
+        barritas.style.display = 'contents';
+        barritas.style.position = 'fixed';
+        barritas.style.top = '0';
+    }
+});
